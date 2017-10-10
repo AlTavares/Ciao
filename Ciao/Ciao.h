1fe5b36551edd39287df65dba135375a1309a15e
Alexandre Mantovani Tavares <alexandre@live.in>
2017-10-10 11:20:43 -0300

    chore: create project

---
+++ Ciao/Ciao.h
@@ -0,0 +1,19 @@
+//
+//  Ciao.h
+//  Ciao
+//
+//  Created by Alexandre Tavares on 10/10/17.
+//  Copyright © 2017 Tavares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ciao.
+FOUNDATION_EXPORT double CiaoVersionNumber;
+
+//! Project version string for Ciao.
+FOUNDATION_EXPORT const unsigned char CiaoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ciao/PublicHeader.h>
+
+
