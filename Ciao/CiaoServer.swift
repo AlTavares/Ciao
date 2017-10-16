0d182d2229aa43cdd08c0c5ef48965a25556d97e
Alexandre Mantovani Tavares <alexandre@live.in>
2017-10-16 13:48:17 -0200

    feat: feedback for server start

--- Ciao/CiaoServer.swift
+++ Ciao/CiaoServer.swift
@@ -11,6 +11,12 @@ import Foundation
 public class CiaoServer {
     var netService: NetService
     var delegate: CiaoServerDelegate?
+    var successCallback: ((Bool) -> Void)?
+    var started = false {
+        didSet {
+            successCallback?(started)
+        }
+    }
     public var txtRecord: [String: String]? {
         get {
             guard let data = netService.txtRecordData() else { return nil }
@@ -25,8 +31,12 @@ public class CiaoServer {
         netService = NetService(domain: domain, type: "_\(type)._tcp", name: name)
         delegate = CiaoServerDelegate()
         netService.delegate = delegate
+    }
+
+    public func start(success: ((Bool) -> Void)? = nil) {
         netService.schedule(in: RunLoop.current, forMode: RunLoopMode.commonModes)
         netService.publish(options: NetService.Options.listenForConnections)
+        successCallback = success
     }
 
     public func stop() {
@@ -39,4 +49,20 @@ public class CiaoServer {
 }
 
 class CiaoServerDelegate: NSObject, NetServiceDelegate {
+    weak var server: CiaoServer?
+
+    func netServiceDidPublish(_ sender: NetService) {
+        server?.started = true
+        Logger.verbose("CiaoServer Started")
+    }
+
+    func netService(_ sender: NetService, didNotPublish errorDict: [String : NSNumber]) {
+        server?.started = false
+        Logger.error("CiaoServer did not publish", errorDict)
+    }
+
+    func netServiceDidStop(_ sender: NetService) {
+        server?.started = false
+        Logger.verbose("CiaoServer Stoped")
+    }
 }
