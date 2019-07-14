d57e3cd38ad6f2fb6efdd51bcb133dc7fa879a4b
Alexandre Mantovani Tavares <alexandre@live.in>
2019-07-14 14:02:54 -0300

    chore: update package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,7 +10,7 @@ let package = Package(
         .library(
             name: "Ciao",
             targets: ["Ciao"]),
-        ],
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -24,5 +24,5 @@ let package = Package(
         .testTarget(
             name: "CiaoTests",
             dependencies: ["Ciao"]),
-        ]
+    ]
 )
