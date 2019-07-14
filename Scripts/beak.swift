8700b1c05c640bcbcbb33b79009dca0a50b59851
Alexandre Mantovani Tavares <alexandre@live.in>
2019-07-14 14:20:10 -0300

    chore: update version

--- Scripts/beak.swift
+++ Scripts/beak.swift
@@ -1,4 +1,4 @@
-// beak: AlTavares/Ciao @ 1.0.2
+// beak: AlTavares/Ciao @ branch:develop
 
 import Foundation
 import Ciao
@@ -9,5 +9,13 @@ public func start() {
         print("Server started:", success)
     }
     ciaoServer.txtRecord = ["someKey": "someValue"]
+    RunLoop.main.run()
+}
+
+public func startHTTP() {
+    let ciaoServer = CiaoServer(type: ServiceType.tcp("http"), name: "ciao", port: 8080)
+    ciaoServer.start { success in
+        print("Server started:", success)
+    }
     RunLoop.main.run()
 }
