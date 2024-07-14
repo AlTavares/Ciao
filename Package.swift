79b9751ddd82fe2a8b7ca7031d55120b4877dc32
Josh Billions <joshbillions@mac.com>
2024-07-14 13:15:37 -0700

    Convert CiaoBrowser to use AsyncStream

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Ciao",
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
---
+++ Sources/Ciao/ServiceConvertible.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Josh Billions on 7/14/24.
+//
+
+import Foundation
+
+public protocol ServiceConvertible {
+    init(service: NetService)
+}
